fd0e9fa18c51a3110b85eafe2b47114fd48e3b0c
Adeyemi Favour O <adeyemipelumi202@gmail.com>
2025-04-05 12:04:36 +0100

    I have work on the sig in page

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_recipe/routes.dart';
-import 'package:food_recipe/utils/theme.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -45,7 +44,7 @@ class _AppState extends State<App> {
           //   scaffoldBackgroundColor: Colors.black,
           //   canvasColor: MaterialTheme.darkScheme().surface,
           // ),
-          initialRoute: AppRoutes.signup,
+          initialRoute: AppRoutes.signin,
           routes: AppRoutes.routes,
           builder: (context, child) {
             return MediaQuery(
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:food_recipe/features/authentication/SignUp/sign_up.dart';
-
+import 'package:food_recipe/features/authentication/sign_in_page.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
-    signup: (context) => const SignupScreen(),
+    signin: (context) => const SignInScreen(),
   };
 
   static String splash = '/';
   static String signup = '/signup';
-
+  static String signin = '/signin';
 }
