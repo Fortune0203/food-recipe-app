f48f676122c493a764d45c5fc1868ea6a9cf0d07
Adeyemi Favour O <adeyemipelumi202@gmail.com>
2025-04-11 20:10:20 +0100

    worked on the notification page, kindly go through it and let me know if you have any suggestions

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,7 +4,10 @@ import 'package:food_recipe/features/authentication/sign_in_page.dart';
 import 'package:food_recipe/features/authentication/splashscreen.dart';
 import 'package:food_recipe/features/home-screen/home_screen.dart';
 import 'package:food_recipe/features/ingrident-screen/ingrident_screen.dart';
+import 'package:food_recipe/features/notification/notification_page.dart';
+import 'package:food_recipe/features/notifications/notifications_page.dart';
 import 'package:food_recipe/features/review-screen/review_screen.dart';
+import 'package:food_recipe/features/saved-recipes/saved_recipes.dart';
 import 'package:food_recipe/features/search-recipe/search_recipe.dart';
 
 class AppRoutes {
@@ -16,6 +19,8 @@ class AppRoutes {
     searchRecipe: (context) => const SearchRecipe(),
     ingredientScreen: (context) => const IngredientScreen(),
     reviewScreen: (context) => const ReviewScreen(),
+    savedRecipe: (context) => const SavedRecipes(),
+    notifications: (context) => const NotificationsPage(),
   };
 
   static String splash = '/';
@@ -25,4 +30,6 @@ class AppRoutes {
   static String searchRecipe = '/search-recipe';
   static String ingredientScreen = '/ingridient-screen';
   static String reviewScreen = '/review-screen';
+  static String savedRecipe = '/saved-recipe';
+  static String notifications = '/notifications';
 }
